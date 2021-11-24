2d5b4cddaa1c7dc610dc7e675520043b407714f6
susji <susji@protonmail.com>
2021-11-24 11:42:58 +0200

    vm: Better error when arithmetic types clash

--- lisby/vm/vm.py
+++ lisby/vm/vm.py
@@ -183,8 +183,8 @@ class VM:
                 result = Float(op(left.value, right.value))  # type: ignore
             else:
                 raise ArithmeticError(
-                    "cannot add types %s and %s"
-                    % (type(left).__name__, type(right).__name__)
+                    "cannot %s types %s and %s: %s vs. %s"
+                    % (str(op), type(left).__name__, type(right).__name__, left, right)
                 )  # type: ignore
             self.stack[-1] = result
 
